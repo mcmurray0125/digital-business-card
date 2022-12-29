7b0f641808bf565ce6e838f60fa2cf834b61b964
Michael Murray <mcmurray0125@gmail.com>
2022-12-28 21:45:28 -0600

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '/src/index.css'
+
+export default function Navbar() {
+    return (
+    <header>
+      <nav className='nav'>
+        <img src="/src/assets/react.svg" alt="" />
+        <h1>Hello World</h1>
+      </nav>
+    </header>
+    )
+  }
